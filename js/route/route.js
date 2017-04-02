e800bc23b467e0923cf8b183decfa45b0f641fb3
TienNguyen <npt1995.khtn@gmail.com>
2017-04-02 22:23:08 +0700

    fix index

---
+++ js/route/route.js
@@ -0,0 +1,6 @@
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', { templateUrl: '/index.html' })
+        .when('/aboutus', { templateUrl: '/html/aboutus.html' })
+
+});
